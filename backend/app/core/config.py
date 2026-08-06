0aad80afd8d443876f422c72babfc35210bf97a7
Michel Escobar <michelejcs@hotmail.com>
2026-08-06 23:11:15 +0200

    Fixed CORs issue

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -26,7 +26,7 @@ class Settings(BaseSettings):
         "http://wickednas:5173",
         "http://wickednas:8000",
     ]
-    CORS_ORIGIN_REGEX: str = r"^https?://(localhost|127\.0\.0\.1|wickednas|10(?:\.\d{1,3}){3}|192\.168(?:\.\d{1,3}){2}|172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?::\d+)?$"
+    CORS_ORIGIN_REGEX: str = r"^https?://(localhost|127\.0\.0\.1|wickednas|(?:\d{1,3}\.){3}\d{1,3})(?::\d+)?$"
     
     # API settings
     API_HOST: str = "0.0.0.0"
